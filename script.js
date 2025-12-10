59dbc1ad254d372e49b2640f9a8ff4acdedea426
J <joao.adsbrasil@gmail.com>
2025-12-10 02:19:17 -0300

    Entendendo os proximos passos

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+/* 
+
+document -> HTML
+
+getElementById -> Trás UM elemento pelo ID
+getElementByClassName -> Trás TODOS os elementos com essa classe
+getElementByTagName -> Trás TODOS os elementos com essa TAG
+getElementByName -> Trás TODOS os elementos com esse NAME
+
+querySelector -> Trás UM elemento, o PRIMEIRO que encontrar
+querySelectorAll -> Trás TODOS os elementos que encontrar
+*/
+
+
 console.log("Olá, agora estou por aqui!")
