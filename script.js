229e6054f3d3cc706327ee52182fdd14b3731c32
J <joao.adsbrasil@gmail.com>
2025-12-11 00:27:37 -0300

    Desafio feito!

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ EVENTOS
 */
 
 const input = document.querySelector("#main-input")
+const paragrafo = document.querySelector(".paragraph-js")
 
 function cliqueiNoBotao(){
-    console.log(input.value)
+    paragrafo.textContent = input.value
 }
 
 function digiteiNoInput(){
